e03d3b6246e185236c33ab7961c606a178315a4c
Akash Tiwari <154492266+shounak-tiwari@users.noreply.github.com>
2026-01-09 13:49:58 +0530

    Add files via upload

---
+++ 01-05/Tuple1.py
@@ -0,0 +1,18 @@
+# Tuple : Tuple is immutable data structure in python in tuple we are not able perform
+#  type of operations like remove  update , create insert ...
+# In Tuple elements is arrange same as list in ordered manner , it also has (+ve and -ve)
+# indexing but not able to change values like list you can access only 
+
+"""
+Tuple 
+Tuple is immutable ordered data structure which store multiple similar and disimilar elements 
+like list but it can not perform curd operation in element , it store their elements inside 
+parenthesis () and sperated by commas... 
+"""
+
+Tup1 = (1,2,3,4,5)
+print(Tup1)
+print(Tup1[0:])
+Tup1[0] = "Sneha"
+print(Tup1)
+# Complete all list assignment and string assignments 
